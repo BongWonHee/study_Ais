b4699bc573a819bee6a3b3f980f75583f0ba42f8
BongWonHee <leeban1004@gmail.com>
2023-08-28 13:15:16 +0900

    수정완

--- codes/MLs/Regressions/RecurrenceOfSurgery_regression_quest.py
+++ codes/MLs/Regressions/RecurrenceOfSurgery_regression_quest.py
@@ -1,10 +1,18 @@
-Sugery_time = float(input('수술시간 : '))
-Hospitalization_period = int(input('입원기간 : '))
-
+import os
 import pickle
+file_path = 'datasets/RecurrenceOfSurgery_regression_quest.plk'
 
-with open ('datasets/RecurrenceOfSurgery_regression_quest.plk','rb') as regression_file :
-    loaded_model = pickle.load(regression_file)
-    input_labels = [[Sugery_time,Hospitalization_period]]
-    regression_predict = loaded_model.predict(input_labels)
-    print('Age Result : {}'.format(regression_predict))
+if os.path.exists(file_path) :
+
+    Sugery_time = float(input('수술시간 : '))
+    Hospitalization_period = int(input('입원기간 : '))
+
+
+
+    with open (file_path,'rb') as regression_file :
+        loaded_model = pickle.load(regression_file)
+        input_labels = [[Sugery_time,Hospitalization_period]]
+        regression_predict = loaded_model.predict(input_labels)
+        print('Age Result : {}'.format(regression_predict))
+else :
+    print('file not found!!')
